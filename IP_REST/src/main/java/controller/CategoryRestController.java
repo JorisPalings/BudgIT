ab0e9c5064441ff05e4c3ef1c12a0a15691fbfd4
r0377429 <r0377429@8fd54e0e-a022-49e7-9f1f-82a2c8f0728b>
2016-05-24 17:36:57 +0000

    - Added "DELETE" to @CrossOrigin methods

--- IP_REST/src/main/java/controller/CategoryRestController.java
+++ IP_REST/src/main/java/controller/CategoryRestController.java
@@ -78,7 +78,7 @@ public class CategoryRestController {
     }
     
     // DELETE category catId
-    @CrossOrigin
+    @CrossOrigin(methods = RequestMethod.DELETE)
     @RequestMapping(value="/categories/{catId}", method = RequestMethod.DELETE)
     public ResponseEntity<Category> deleteCategory(@PathVariable int catId) {
         Category category = application.getCategory(catId);
