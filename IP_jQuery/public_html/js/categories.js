bda3ebe11091a2c5f6563395e051a61ed5e7a8d9
r0377429 <r0377429@8fd54e0e-a022-49e7-9f1f-82a2c8f0728b>
2016-05-25 04:59:31 +0000

    - Removed commented-out, non-implemented methods

--- IP_jQuery/public_html/js/categories.js
+++ IP_jQuery/public_html/js/categories.js
@@ -7,9 +7,6 @@ $(document).ready(function() {
             $("main").append("<section>");
             var newestSection = $("main section:last-of-type");
             newestSection.append("<a class='rest' href='/IP_REST/categories/" + this.id + "'><img class='options' src='images/delete.png' alt='Delete'></a>");
-            //newestSection.append("<a class='rest' href='/IP_SpringMVC/categories/0/edit.htm'><img class='options' src='images/edit.png' alt='Edit'></a>");
-            //newestSection.append("<a class='rest' href='/IP_SpringMVC/categories/0/expenses/new.htm'><img class='options' src='images/add.png' alt='Add'></a>");
-            //newestSection.append("<a class='rest' href='/IP_SpringMVC/categories/0/expenses/view.htm'><img class='options' src='images/view.png' alt='View'></a>");
             newestSection.append("<h2 class='toggleable'>" + this.name + "</h2>");
             newestSection.append("<dl></dl>");
             var newestSectionExpenses = $("main section:last-of-type dl")
